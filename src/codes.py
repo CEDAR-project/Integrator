48e29021857babcb2809691f308623d65d7f0f7f
Christophe Gueret <christophe.gueret@gmail.com>
2014-03-21 08:02:05 +0100

    Added code to create rules to harmonize on marital status, occupation position and sex on both columns and rows

---
+++ src/codes.py
@@ -0,0 +1,76 @@
+from rdflib import ConjunctiveGraph, Namespace, Literal, RDF, RDFS, BNode, URIRef
+import csv
+
+class Codes(object):
+    namespaces = {
+        'dcterms':Namespace('http://purl.org/dc/terms/'), 
+        'skos':Namespace('http://www.w3.org/2004/02/skos/core#'), 
+        'tablink':Namespace('http://example.org/ns#'), 
+        'harmonizer':Namespace('http://harmonizer.example.org/ns#'),
+        'rules':Namespace('http://rules.example.org/resource/'),
+        'qb':Namespace('http://purl.org/linked-data/cube#'), 
+        'owl':Namespace('http://www.w3.org/2002/07/owl#'),
+        'sdmx-dimension':Namespace('http://purl.org/linked-data/sdmx/2009/dimension#'),
+        'sdmx-code':Namespace('http://purl.org/linked-data/sdmx/2009/code#'),
+        'cedar':Namespace('http://cedar.example.org/ns#')
+    }
+    
+    def __init__(self):
+        # Declare the dimensions
+        self.dimensions = {
+            self.namespaces['sdmx-dimension']['sex'] : {
+                'fileName' : 'codes/sex.csv',
+                'map' : dict()
+            },
+            self.namespaces['cedar']['maritalStatus'] : {
+                'fileName' : 'codes/marital_status.csv',
+                'map' : dict()
+            },
+            self.namespaces['cedar']['occupationPosition'] : {
+                'fileName' : 'codes/occupation_position.csv',
+                'map' : dict()
+            }
+        }
+        
+        # Load the content of the files
+        for dim in self.dimensions.values():
+            print "Loading %s ..." % dim['fileName']            
+            file  = open(dim['fileName'], "rb")
+            reader = csv.reader(file)
+            header_row = True
+            for row in reader:
+                # Skip the header
+                if header_row:
+                    header_row = False
+                    continue
+                # Skip empty lines
+                if len(row) != 2:
+                    continue
+                if row[1].startswith("http"):
+                    dim['map'][row[0]] = URIRef(row[1])
+                else:
+                    dim['map'][row[0]] = Literal(row[1])
+            file.close()
+            print "Got %d entries" % len(dim['map'].keys())
+    
+    def get_code(self, dimension, literal):
+        """
+        Return the code associated to a literal
+        """
+        if dimension in self.dimensions:
+            if literal in self.dimensions[dimension]['map']:
+                return self.dimensions[dimension]['map'][literal]
+        return None
+
+    def detect_code(self, literal):
+        """
+        Return the first match (dim, value) for the literal
+        """
+        for (dimension,data) in self.dimensions.iteritems():
+            if literal in data['map']:
+                return (dimension, data['map'][literal])
+        return None
+    
+if __name__ == '__main__':
+    codes = Codes()
+    
--- src/make_rules.py
+++ src/make_rules.py
@@ -4,6 +4,9 @@ from rdflib import ConjunctiveGraph, Namespace, Literal, RDF, RDFS, BNode, URIRe
 import re
 import uuid
 import bz2
+import operator
+
+from codes import Codes
 
 class RuleMaker(object):
     namespaces = {
@@ -15,14 +18,16 @@ class RuleMaker(object):
       'qb':Namespace('http://purl.org/linked-data/cube#'), 
       'owl':Namespace('http://www.w3.org/2002/07/owl#'),
       'sdmx-dimension':Namespace('http://purl.org/linked-data/sdmx/2009/dimension#'),
-      'sdmx-code':Namespace('http://purl.org/linked-data/sdmx/2009/code#')
+      'sdmx-code':Namespace('http://purl.org/linked-data/sdmx/2009/code#'),
+      'cedar':Namespace('http://cedar.example.org/ns#')
     }
     
-    def __init__(self, tablename, endpoint, namedgraph):
+    def __init__(self, codes, tablename, endpoint, namedgraph):
         """
         Constructor
         """
         # Keep parameters
+        self.codes = codes
         self.tablename = tablename
         self.endpoint = endpoint
         self.namedgraph = namedgraph
@@ -36,54 +41,157 @@ class RuleMaker(object):
         """
         Start the job
         """
+        print "Start processing %s" % self.tablename
+        
+        #####
+        # Start with the column headers
+        #####
         
         # Get a list of all the column headers
         headers = {}
-        max_depth = 0
         sparql = SPARQLWrapper(self.endpoint)
         query = """
-        select distinct ?header ?label ?parent ?depth from <GRAPH> where {
+        select distinct ?header ?label ?parent from <GRAPH> where {
         ?header a <http://example.org/ns#ColumnHeader>.
         ?header <http://www.w3.org/2004/02/skos/core#prefLabel> ?label.
         ?header <http://example.org/ns#subColHeaderOf> ?parent.
-        ?header <http://example.org/ns#depth> ?depth.
         }
-        """.replace('GRAPH',namedgraph)
+        """.replace('GRAPH',self.namedgraph)
         sparql.setQuery(query)
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
         for result in results["results"]["bindings"]:
-            resource = result['header']['value']
+            resource = URIRef(result['header']['value'])
             headers[resource] = {}
             headers[resource]['label'] = result['label']['value']
-            headers[resource]['parent'] = result['parent']['value']
-            headers[resource]['depth'] = int(result['depth']['value'])
-            if headers[resource]['depth'] > max_depth:
-                max_depth = headers[resource]['depth']
+            headers[resource]['parent'] = URIRef(result['parent']['value'])
+        print "Process %d column headers" % len(headers)
         
+        # Get a sublist of leaves
+        leaves = []
+        for header in headers.keys():
+            ok = True
+            for h in headers.keys():
+                if headers[h]['parent'] == header:
+                    ok = False
+            if ok:
+                leaves.append(header)
+                
         # Process all the leaf headers, one by one
-        for (header, data) in headers.iteritems():
-            # Skip non leaf
-            if data['depth'] != max_depth:
-                continue
-            self.process_header(headers, header, header)
+        for leaf in leaves:
+            self.process_column_header(headers, leaf)
             
-    
-    def process_header(self, headers, leaf, header):
-        # Get the data
-        data = headers[header]
+        #####
+        # Move on to the row headers
+        #####
+        headers = {}
+        sparql = SPARQLWrapper(self.endpoint)
+        query = """
+        select distinct ?header ?label from <GRAPH> where {
+        ?header a <http://example.org/ns#RowProperty>.
+        ?header <http://www.w3.org/2004/02/skos/core#prefLabel> ?label.
+        }
+        """.replace('GRAPH',self.namedgraph)
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        for result in results["results"]["bindings"]:
+            resource = URIRef(result['header']['value'])
+            headers[resource] = {}
+            headers[resource]['label'] = result['label']['value']
+        print "Process %d row properties" % len(headers)
         
-        # Detect several things in the label
-        self.detect_sex(leaf, data['label'])
-        self.detect_sums(leaf, data['label'])
+        for (header, label) in headers.iteritems():
+            self.process_row_header(header, label)
             
-        # Detect total
-        #self.detect_sums(headers)
+        #####
+        # Done
+        #####
+        print "Done"
         
-        # Recurse (added filter to go around vertical merge)
-        if data['depth'] != 1:
-            if headers[data['parent']]['label'] != data['label']:
-                self.process_header(headers, leaf, data['parent'])
+    def process_row_header(self, header, label):
+        """
+        Process a row header.
+        We get a sample of the values found in the rows and guess the type
+        from it
+        """
+        sample = []
+        sparql = SPARQLWrapper(self.endpoint)
+        query = """
+        select distinct ?label from <GRAPH> where {
+        ?obs a <http://purl.org/linked-data/cube#Observation>.
+        ?obs <DIM> ?label.
+        } limit 20
+        """.replace('GRAPH',self.namedgraph).replace('DIM', header)
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        for result in results["results"]["bindings"]:
+            label = self._clean_string(result['label']['value'])
+            if 'totaal' not in label:
+                sample.append(label)
+        
+        # If the sample is empty forget about this header
+        if len(sample) == 0:
+            return
+        
+        # Check if we can find the dimension associated to this header
+        counts = {}
+        for entry in sample:
+            result = codes.detect_code(entry)
+            if result != None:
+                (dim,value) = result
+                counts.setdefault(dim,0)
+                counts[dim] = counts[dim] + 1
+        
+        # If we can't find any possible match, skip this header        
+        if len(counts) == 0:
+            return
+        
+        # Get the dimension with the highest count
+        sorted_counts = sorted(counts.iteritems(), key=operator.itemgetter(1), reverse=True)
+        (dimension,count) = sorted_counts[0]
+                
+        # Tweak: jobPosition can not be in a hierarchical position
+        # verify that the header is not the sub header of something
+        if dimension == self.namespaces['cedar']['occupationPosition']:
+            sparql = SPARQLWrapper(self.endpoint)
+            query = """
+            ask from <GRAPH> {
+            <DIM> <http://example.org/ns#subPropertyOf> ?x.
+            } 
+            """.replace('GRAPH',self.namedgraph).replace('DIM', header)
+            sparql.setQuery(query)
+            sparql.setReturnFormat(JSON)
+            if sparql.query().convert()['boolean']:
+                return
+        
+        # Create a rule to bind the dimension to this header    
+        self.create_rule_set_dimension(header, dimension)
+        
+    def process_column_header(self, headers, header):
+        """
+        Process a column header
+        headers = set of all headers
+        header = target header
+        """
+        # Try to find if it's a "totaal"
+        header_with_total = self.contains_total(headers, header)
+        if header_with_total == None:
+            # The set of dimensions that will be filled in by detect_dimensions
+            dimensions = set()
+            
+            # Try to detect dimensions in this header and those above it
+            self.detect_dimensions(dimensions, headers, header)
+            
+            # Add all the results
+            for dimension in dimensions:
+                (source, dim) = dimension
+                self.create_rule_add_dimension_value(header, source, dim)
+        else:
+            # Add a rule to ignore this observation
+            self.create_rule_ignore_observation(header, header_with_total)
+                
     
     def saveTo(self, filename):
         """
@@ -94,53 +202,83 @@ class RuleMaker(object):
         file.writelines(turtle)
         file.close()
         
-    def detect_sex(self, resource, label):
+    def detect_dimensions(self, dimensions, headers, header):
         """
-        Check for known labels for sex
+        Check for known labels
         """
-        label_clean = self._clean_string(label)
+        # Get the data
+        data = headers[header]
         
-        # Women
-        if label_clean == 'v' or label_clean == 'vrouwen' or label_clean == 'vrouwelijk geslacht':
-            self.create_rule_add_dimension(URIRef(resource),
-                                           self.namespaces['sdmx-dimension']['sex'],
-                                           self.namespaces['sdmx-code']['sex-V'])
-            return True
-
-        # Man            
-        if label_clean == 'm' or label_clean == 'mannen' or label_clean == 'mannelijk geslacht':
-            self.create_rule_add_dimension(URIRef(resource),
-                                           self.namespaces['sdmx-dimension']['sex'],
-                                           self.namespaces['sdmx-code']['sex-M'])
-            return True
-        return False
+        # Clean the label
+        label_clean = self._clean_string(data['label'])
+        
+        # Check if we can find something
+        result = codes.detect_code(label_clean)
+        if result != None:
+            dimensions.add((header, result))
+        
+        # Recurse
+        parent = data['parent']
+        if parent in headers:
+            # Hot fix to skip vertical merge resulting in duplicate headers
+            while parent == header:
+                parent = headers[parent]['parent']
+            if parent in headers:
+                self.detect_dimensions(dimensions, headers, parent)
     
-    def detect_sums(self, resource, label):
+    def contains_total(self, headers, header):
         """
-        Check if the label speaks about "totaal"
+        Check if the header is about the total of something
         """
-        return False
+        # Get the data
+        data = headers[header]
+        
+        # Clean the label
+        label_clean = self._clean_string(data['label'])
+        
+        # Check if the label contains the string "totaal"
+        if "totaal" in label_clean:
+            return header
+        
+        # Recurse to upper level
+        parent = data['parent']
+        if parent in headers:
+            # Hot fix to skip vertical merge resulting in duplicate headers
+            while parent == header:
+                parent = headers[parent]['parent']
+            if parent in headers:
+                return self.contains_total(headers, parent)
+        
+        return None
     
     def _clean_string(self, text):
         """
         Utility function to clean a string
         """
+        # Remove some extra things
         text_clean = text.replace('.', '').replace('_', ' ').lower()
+        # Shrink spaces
         text_clean = re.sub(r'\s+', ' ', text_clean)
+        # Remove lead and trailing whitespaces
+        text_clean = text_clean.strip()
         return text_clean
     
-    def create_rule_add_dimension(self, target, dimension, value):
+    def create_rule_add_dimension_value(self, target, source, dimensionvalue):
         """
         Create a new harmonization rule that assign a dimension and value
-        to all the observations having the target as a dimension
+        to all the observations having the targetDimension as a dimension
         """
+        (dimension, value) = dimensionvalue
         resource = URIRef(self.namespaces['rules'] + str(uuid.uuid1()))
         self.graph.add((resource,
                         RDF.type,
-                        self.namespaces['harmonizer']['AddDimensionRule']))
+                        self.namespaces['harmonizer']['AddDimensionValue']))
         self.graph.add((resource,
-                        self.namespaces['harmonizer']['target'],
+                        self.namespaces['harmonizer']['targetDimension'],
                         target))
+        self.graph.add((resource,
+                        self.namespaces['harmonizer']['generatedFrom'],
+                        source))
         self.graph.add((resource,
                         self.namespaces['harmonizer']['dimension'],
                         dimension))
@@ -148,18 +286,59 @@ class RuleMaker(object):
                         self.namespaces['harmonizer']['value'],
                         value))
     
+    def create_rule_set_dimension(self, target, dimension):
+        """
+        Create a new harmonization rule that assign a dimension to a given
+        header. The actual value will be resolved at the creation of the
+        harmonized cubes
+        """
+        resource = URIRef(self.namespaces['rules'] + str(uuid.uuid1()))
+        self.graph.add((resource,
+                        RDF.type,
+                        self.namespaces['harmonizer']['SetDimension']))
+        self.graph.add((resource,
+                        self.namespaces['harmonizer']['targetDimension'],
+                        target))
+        self.graph.add((resource,
+                        self.namespaces['harmonizer']['dimension'],
+                        dimension))
+        
+    def create_rule_ignore_observation(self, target, source):
+        """
+        Create a new harmonization rule that tells to ignore observations
+        associated to the target dimension
+        """
+        resource = URIRef(self.namespaces['rules'] + str(uuid.uuid1()))
+        self.graph.add((resource,
+                        RDF.type,
+                        self.namespaces['harmonizer']['IgnoreObservation']))
+        self.graph.add((resource,
+                        self.namespaces['harmonizer']['targetDimension'],
+                        target))
+        self.graph.add((resource,
+                        self.namespaces['harmonizer']['generatedFrom'],
+                        source))
+        
 if __name__ == '__main__':
-    #table = 'BRT_1899_10_T_marked'
+    # Parameters
+    ngtemplate = 'http://lod.cedar-project.nl/resource/v2/TABLE'
+    endpoint = 'http://lod.cedar-project.nl:8080/sparql/cedar'
+    
+    # Load the codes
+    codes = Codes()
+    
+    # Get a list of tables (testing : 'BRT_1899_10_T_marked')
     tables = [table.strip() for table in open('tables.txt')]
+    
+    # Process each table one by one
     for table in tables:
         t = table.split('_')
         type = t[0]
         year = t[1]
-        if type != 'BRT' or year != '1920':
-            continue
-        print table
-        namedgraph = 'http://lod.cedar-project.nl/resource/v2/TABLE'.replace('TABLE', table)
-        r = RuleMaker(table, 'http://lod.cedar-project.nl:8080/sparql/cedar', namedgraph)
+        # Skip some tables for testing
+        #if type != 'BRT' or year != '1920':
+        #    continue
+        r = RuleMaker(codes, table, endpoint, ngtemplate.replace('TABLE', table))
         r.go()
         r.saveTo('rules/' + table + '.ttl.bz2')
         #exit(0)
