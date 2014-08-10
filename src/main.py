3f074d50b05b1f74f12ea94964f65dce55952d6c
Christophe Gueret <christophe.gueret@gmail.com>
2014-08-10 09:22:19 +0200

    Small refactor

--- src/main.py
+++ src/main.py
@@ -3,14 +3,14 @@ from common.configuration import Configuration, RAW_XLS_PATH, MARKING_PATH,\
     RAW_RDF_PATH
 import glob
 import os
+import bz2
 from tablink import TabLinker
+from push import Pusher
 
-if __name__ == '__main__':
-    config = Configuration('config.ini')
-
-    #
-    # Convert the raw xls files into raw RDF data cubes
-    #
+def generate_raw_rdf(config):
+    '''
+    Convert the raw xls files into raw RDF data cubes
+    '''
     raw_xls_files = glob.glob(config.getPath(RAW_XLS_PATH))
     marking_files = glob.glob(config.getPath(MARKING_PATH))
     marking_index = {}
@@ -22,6 +22,8 @@ if __name__ == '__main__':
         if name in marking_index:
             marking_file = marking_index[name]
             dataFile = config.getPath(RAW_RDF_PATH) + name + '.ttl'
+            if config.isCompress():
+                dataFile = dataFile + '.bz2'
             if not os.path.exists(dataFile):
                 print name
                 try:
@@ -29,4 +31,29 @@ if __name__ == '__main__':
                     tLinker.doLink()
                 except:
                     print "Error !"
-                    
+
+def push_raw_rdf_to_virtuoso(config):
+    '''
+    Push the raw rdf graphs to virtuoso
+    '''
+    raw_rdf_files = glob.glob(config.getPath(RAW_RDF_PATH) + '/*')
+    for raw_rdf_file in sorted(raw_rdf_files):
+        name = os.path.basename(raw_rdf_file).split('.')[0]
+        named_graph = 'urn:graph:cedar:raw-rdf:' + name
+        data_file = raw_rdf_file
+        if data_file.endswith('.bz2'):
+            uncompressedData = bz2.BZ2File(data_file).read()
+            f = open('/tmp/graph.ttl', 'wb')
+            f.write(uncompressedData)
+            f.close()
+            data_file = '/tmp/graph.ttl'
+        print named_graph
+        pusher = Pusher(named_graph, data_file)
+        pusher.upload_graph()
+        
+if __name__ == '__main__':
+    config = Configuration('config.ini')
+
+    #generate_raw_rdf(config)
+    #push_raw_rdf_to_virtuoso(config)
+    
---
+++ src/push.py
@@ -0,0 +1,48 @@
+import pycurl
+
+SPARQL = "http://lod.cedar-project.nl:8080/sparql"
+SERVER = "http://lod.cedar-project.nl:8080/sparql-graph-crud"
+
+class Pusher(object):
+    def __init__(self, uri, turle_file):
+        self.user = ':'.join([c.strip() for c in open('credentials-virtuoso.txt')])
+        self.uri = uri
+        self.turle_file = turle_file
+        
+    def upload_graph(self):
+        # Clear the previous graph
+        c = pycurl.Curl()
+        values = [("query", "CLEAR GRAPH <%s>" % self.uri)]
+        c.setopt(c.URL, SPARQL)
+        c.setopt(c.USERPWD, self.user)
+        c.setopt(c.HTTPPOST, values)
+        c.setopt(pycurl.WRITEFUNCTION, lambda x: None)
+        c.perform()
+        c.close()
+    
+        # Upload the new data    
+        c = pycurl.Curl()
+        values = [("res-file", (pycurl.FORM_FILE, self.turle_file)),("graph-uri", self.uri)]
+        c.setopt(c.URL, SERVER)
+        c.setopt(c.USERPWD, self.user)
+        c.setopt(c.HTTPPOST, values)
+        c.setopt(c.HTTPHEADER, [ 'Content-Type:multipart/form-data', 'Expect: ']);
+        c.setopt(pycurl.WRITEFUNCTION, lambda x: None)
+        c.perform()
+        c.close()
+    
+if __name__ == '__main__':
+    data = "/tmp/data.ttl"
+    graph = "urn:graph:update:test:put"
+    pusher = Pusher(graph, data)
+    pusher.upload_graph()
+
+
+# Push to OWLIM
+# curl -X POST -H "Content-Type:application/x-turtle" -T config.ttl http://localhost:8080/openrdf-sesame/repositories/SYSTEM/rdf-graphs/service?graph=http://example.com#g1
+# https://github.com/LATC/24-7-platform/blob/master/latc-platform/console/src/main/python/upload_files.py
+# http://owlim.ontotext.com/display/OWLIMv52/OWLIM+FAQ
+
+# Push to Virtuoso
+# http://virtuoso.openlinksw.com/dataspace/doc/dav/wiki/Main/VirtGraphProtocolCURLExamples
+
