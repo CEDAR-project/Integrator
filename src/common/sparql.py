fd5c2160ec09b18050e7da7930864b531d98efef
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2015-05-19 16:34:45 +0200

    Id

--- src/common/sparql.py
+++ src/common/sparql.py
@@ -24,6 +24,7 @@ class SPARQLWrap(object):
                 query = query.replace(k,v)
         query = self.prefixes + query
         
+        print query
         sparql.setQuery(query)
         sparql.setReturnFormat(JSON)
         #sparql.setCredentials('rdfread', 'red_fred')
